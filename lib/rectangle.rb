15a92be322b3342e65ff2b3e2dd7479ae133ce8b
Smit Shah <who828@gmail.com>
2012-08-03 11:53:28 +0530

    It calculates the perimeter of rectangle

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,9 +1,14 @@
+#A quadrilateral with opposite right anglesk
 class Rectangle
-  attr_reader :width, :height
+  attr_reader :width, :height, :sides
 
   def initialize(width, height)
     @width = width
     @height = height
+    @sides = 4
   end
 
+  def perimeter
+    2*(width + height)
+  end
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -5,10 +5,12 @@ describe Rectangle do
     rect = Rectangle.new(10, 5)
     rect.width.should == 10
     rect.height.should == 5
+    rect.sides.should == 4
   end
-  it "should have 4 sides" do
-    rect = Rectangle.new
-    rect.sides.should == 4 
+
+  it "calculates the perimeter" do
+    rect = Rectangle.new(10,20)
+    rect.perimeter.should == 60
   end
 end
 
